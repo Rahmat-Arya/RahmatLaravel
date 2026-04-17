eec65f2e81e3a2de60ff0b2b9b794da1a19cf4c2
Rahmat-Arya <sparkleinthenight@gmail.com>
2026-04-17 15:08:48 +0700

    well

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,15 @@ Route::get('/', function () {
 Route::get('/tes', function () {
     return view('tes');
 });
+ 
+Route::get('/bjir', function () {
+    return 'TES WELL AHHHHHH';
+});
+
+Route::get('/user/{id}', function ($id) {
+    return "Profile User dengan ID:" . $id;
+});
+
+Route::get('/produk/{slug?}', function ($slug = 'semua-produk') {
+    return "Menampilkan: " . $slug;
+});
